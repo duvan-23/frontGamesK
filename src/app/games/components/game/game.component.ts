09715ea63a3ce617e43f9ae2bb52a22115c1fb95
duvan-23 <duvamendi2@hotmail.com>
2025-01-12 01:56:52 -0500

    game component

---
+++ src/app/games/components/game/game.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
+import { Game } from 'app/games/models/game';
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [RouterLinkWithHref, CommonModule],
+  templateUrl: './game.component.html'
+})
+export class GameComponent {
+  @Input ({required: true}) game!: Game;
+
+  seeDetails() {
+
+  }
+}
