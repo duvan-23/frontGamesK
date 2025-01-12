8720d8fe7f46b61da6b69413f0de29e095b25788
duvan-23 <duvamendi2@hotmail.com>
2025-01-11 22:52:41 -0500

    interceptor

---
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '@shared/services/auth.service';
+import { environment } from '@enviroment';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const token = authService.token();
+  
+  if (token && !(environment.URL_EXCEPTION.some((url) => req.url.includes(url)))) {
+    // Clone the request and add the Authorization header with the Bearer token
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  }
+  return next(req);
+};
