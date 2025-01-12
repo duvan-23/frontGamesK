483ee219606494a82129281ea4d0bf6a0ca79563
duvan-23 <duvamendi2@hotmail.com>
2025-01-12 18:39:35 -0500

    nav router active and prevent reload

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,16 +1,25 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './nav.component.html'
 })
 export class NavComponent {
   menuOpen = false;
+  private router = inject(Router);
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+  
+
+  preventReload(event: Event, targetUrl: string): void {
+    if (this.router.url === targetUrl) {
+      event.preventDefault(); // Prevent the default navigation
+    }
   }
 }
