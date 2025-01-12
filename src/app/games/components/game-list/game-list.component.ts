35c0409b1196bd8301866566c8eb47e0f51fae85
duvan-23 <duvamendi2@hotmail.com>
2025-01-12 02:02:09 -0500

    list of games component

---
+++ src/app/games/components/game-list/game-list.component.ts
@@ -0,0 +1,61 @@
+import { Component, inject, signal } from '@angular/core';
+import { AuthService } from '@shared/services/auth.service';
+import { GamesService } from 'app/games/services/games.service';
+import { GameComponent } from '../game/game.component';
+import { PaginatorComponent } from '../paginator/paginator.component';
+import { SearchComponent } from '../search/search.component';
+
+
+@Component({
+  selector: 'app-game-list',
+  standalone: true,
+  imports: [GameComponent, PaginatorComponent, SearchComponent],
+  templateUrl: './game-list.component.html'
+})
+export class GameListComponent {
+ 
+
+  authService = inject(AuthService);
+  gamesService = inject(GamesService);
+
+  pageNumber = this.gamesService.pageNumber;
+  pageTotal = this.gamesService.pageTotal;
+  pageSize =  this.gamesService.pageSize; 
+  games=this.gamesService.games;
+  ngOnInit(): void {
+    this.authService.login().subscribe(
+      () => {
+        this.getGames();
+      }
+    );
+  }
+  getGames(){
+    this.gamesService.getGames().subscribe({
+      next:(data) => {
+        this.gamesService.games.set(data);
+        this.gamesService.filterDataByPage(this.pageNumber());
+        this.gamesService.pageTotal.set(Math.ceil(this.games().length / this.pageSize));
+      },
+      error:(e)=>{
+        console.log(e);
+      }
+    });
+  }
+
+  onPageChange(pageNumber: number): void {
+    this.filterData(pageNumber);
+  }
+
+  filterData(pageNumber: number){
+    this.gamesService.filterDataByPage(pageNumber);
+    this.pageNumber.set(this.gamesService.pageNumber());
+  }
+
+  searchName(title:any){
+    if(title != ""){
+      this.gamesService.filterDataByName(title);
+    }else{
+      this.getGames();
+    }
+  }
+}
