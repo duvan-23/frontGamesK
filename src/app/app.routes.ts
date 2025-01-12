1a25cdfa3087b93425c98f00dfe7701f82eab3a9
duvan-23 <duvamendi2@hotmail.com>
2025-01-11 22:53:57 -0500

    router pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { GameListComponent } from './games/components/game-list/game-list.component';
+import { LayoutComponent } from '@shared/components/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    component: LayoutComponent,
+    children: [
+        {
+            path: '', component: GameListComponent
+        }
+    ]
+}];
