1832f94ccb48c316619fbe4cbce2d3c9cbba1582
duvan-23 <duvamendi2@hotmail.com>
2025-01-11 22:51:42 -0500

    variables

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,7 @@
 export const environment = {
     production: false,
+    API_URL: 'http://localhost:3000/',
+    USERLOGIN : "admin",
+    PASSWORDLOGIN : "5678",
+    URL_EXCEPTION : ['auth/login']
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,7 @@
 export const environment = {
     production: true,
+    API_URL: 'http://localhost:3000/',
+    USERLOGIN : "admin",
+    PASSWORDLOGIN : "5678",
+    URL_EXCEPTION : ['auth/login']
 };
