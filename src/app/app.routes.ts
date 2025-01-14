85a5e048146400b552417ed002798fcc4363c4e9
duvan-23 <duvamendi2@hotmail.com>
2025-01-14 00:58:09 -0500

    adding login route and guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,31 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from '@shared/components/layout/layout.component';
 import { HomeComponent } from './home/pages/home/home.component';
 import { SlotMachineComponent } from './slot-machine/pages/slot-machine/slot-machine.component';
+import { LoginComponent } from './login/pages/login/login.component';
+import { authGuard } from './guards/auth.guard';
+import { loginGuard } from './guards/login.guard';
 
-export const routes: Routes = [{
-    path: '',
-    component: LayoutComponent,
-    children: [
-        {
-            path: '', component: HomeComponent
-        },
-        {
-            path: 'slot-machine', component: SlotMachineComponent
-        },
-    ]
-}];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        canActivate: [authGuard],
+        children: [
+            {
+                path: '', component: HomeComponent
+            },
+            {
+                path: 'slot-machine', component: SlotMachineComponent
+            },
+        ]
+    },
+    {
+        path: 'login',
+        canActivate: [loginGuard],
+        component: LoginComponent
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    },
+];
