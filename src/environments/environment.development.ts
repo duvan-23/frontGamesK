0dac6f7a526fd97b2c207e27a01534df013a72ff
duvan-23 <duvamendi2@hotmail.com>
2025-01-15 01:36:31 -0500

    delete user and password login

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,5 @@
 export const environment = {
     production: false,
     API_URL: 'http://localhost:3000/',
-    USERLOGIN : "admin",
-    PASSWORDLOGIN : "5678",
     URL_EXCEPTION : ['auth/login']
 };
