b9b1e9058a0278f4f9aa5c06639f3311bbae6385
duvan-23 <duvamendi2@hotmail.com>
2025-01-16 18:38:37 -0500

    fix bug guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,10 +1,16 @@
-import { inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID, inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '@shared/services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   let router = inject(Router);
   let authService = inject(AuthService);
+  let  platformId = inject(PLATFORM_ID);
+  
+  if (!isPlatformBrowser(platformId)) {
+    return true;
+  }
   //Check if the token exist and is valid
   const isValidToken = authService.isValidToken();
   if (!isValidToken) {
